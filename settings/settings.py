b6bd981aab5e96b74ceb002ec9f5c2fdcebf259a
alexpetralia <alex.petralia@gmail.com>
2015-12-10 18:49:04 -0500

    updates

--- settings/settings.py
+++ settings/settings.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__ + "\\.."))
+BASE_DIR = os.path.dirname(os.path.abspath(os.getcwd()))
 DATABASE_PATH = BASE_DIR + "\\mbta_trips.db"
 
 TABLE_ONE = "completed_trip_history"
